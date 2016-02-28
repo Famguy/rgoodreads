49cb2943570610dc24ccd39e60147d691fb9e538
Sagun Pai <sagung.pai@gmail.com>
2016-02-28 16:36:34 +0530

    Author by name mods

--- R/author_by_name.R
+++ R/author_by_name.R
@@ -1,7 +1,12 @@
 author_by_name <- function(name) {
-    tbl <- NULL
-    ggr <- goodreads_GET (paste0('api/author_url/',name))
-    tbl <- goodreads_parse_user(ggr)
-    tbl
+    d <- NULL
+    ggr <- goodreads_GET (paste0('api/author_url/',RCurl::curlEscape(name)))
+    text <- httr::content(ggr, as = "text")
+    if (identical(text, "")) stop("No output to parse", call. = FALSE)
+    k <- regexpr('author id=\"(\\d+)\"',text, TRUE)
+    l <- substr(text,k,k+attr(k,"match.length")-1)
+    m <- gsub("author id=", "", l)
+    n <- gsub("\"","",m)
+    author(n)
 }
 
